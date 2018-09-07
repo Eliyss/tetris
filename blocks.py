b7e53b863d0de5d09bcec5f74980c2fe8b06e718
Kevin Zhu <aikevinzhu@gmail.com>
2018-09-06 23:24:31 -0400

    started board logic, added more block functions, renamed files

--- blocks.py
+++ blocks.py
@@ -1,7 +1,7 @@
 from PyQt5.q
 
-class block(object):
-    blockCoords =   (
+class Shape(object):
+    shapeCoords =   (
         ((0, 0),    (0, 0),     (0, 0),     (0, 0)),
         ((-1, 0),   (0, 0),     (1, 0),     (2, 0)), #I
         ((-1, 1),   (-1, 0),    (0, 0),     (1, 0)),
@@ -13,7 +13,7 @@ class block(object):
         ((0, 0),    (0, 0),     (0, 0),     (0, 0))
     )
 
-    blankShape = 0
+    empty = 0
     I = 1
     J = 2
     L = 3
@@ -24,24 +24,34 @@ class block(object):
 
 
     def __init__(self):
-        self.shape = blankShape
+        self.shape = empty
         self.coords = [[0, 0] for i in range(4)]
 
 
     def getShape(self):
         return self.shape
     
-    def createShape(self, shape):
+    def setShape(self, shape):
         self.shape = shape
-        coords = blockCoords[shape]
+        coords = shapeCoords[shape]
         for i in range(4):
             for j in range(2):
                 self.coords[i][j] = coords[i][j]
 
-    def height(self):
-        
+    def setRandomShape(self):
+        self.setShape(random.randint(1, 7))
 
+    def getX(self, i):
+        return self.coords[i][0]
 
+    def setX(self, i, newX):
+        self.coords[i][0] = newX
+
+    def getY(self, i):
+        return self.coords[i][1]
+
+    def setY(self, i, newY):
+        self.coords[i][1] = newY
 
     def rotateRight(self):
         if self.shape == O:
@@ -53,5 +63,3 @@ class block(object):
 
         for i in range(4):
             rotated.
-
-
--- board.py
+++ board.py
@@ -1,6 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QFrame
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor 
+import sys, random
+import blocks
+import shapes
 
-# we want a pause, play, hold/swap, next shape, score, 
+# we want a pause, play, hold/swap, next shape, score, rotate bounce
 
 class tetris(QMainWindow)
 
@@ -15,21 +20,116 @@ class board(QFrame):
     boardWidth = 10
     boardHeight = 22
     tickRate = 500
+    squareSize = 50
 
     def __init__(self, parent):
         super().__init__(parent)
         self.initBoard()
     
     def initBoard(self):
-        self.
+        self.currentShape = blocks.Shape()
+        self.squareGrid = [][]
+        self.currentX = 0
+        self.currentY = 0
+       
 
-
-    def tick
-
-    def spawnShape()
-
+    def getShapeAt(self, x, y):
+        return self.squareGrid[x][y]
     
+    def setShapeAt(self, x, y, shape):
+        self.squareGrid[x][y] = shape
 
-        
+    def spawnShape(self):
+        self.currentShape = blocks.Shape()
+        self.currentShape.setRandomShape
+        self.currentX = 4
+        self.currentY = 20
+        self.totalLinesCleared = 0
+
+        if not self.placeBlock(self.currentShape, self.currentX, self.currentY):
+            #game is over
+    
+    def placeBlock(self, shape, newX, newY):
+        for i in range(4):
+            x = newX + shape.getX(i)
+            y = newX + shape.getY(i)
+
+            if x < 0 or x > 9 or y < 0 or y > 21:
+                return False
+            if self.getShapeAt(x, y) != blocks.empty:
+                return False
+            
+        self.currentShape = shape
+        self.currentX = newX
+        self.currentY = newY
+        self.updateBoard()
+
+       return True
+    
+    def downOne(self):
+        if not self.placeBlock(self.curPiece, self.curX, self.curY - 1):
+            self.reachedBottom()
+
+    def smash(self):
+        tempY = self.currentY
+        while tempT > 0:
+            if placeBlock(self.currentShape, self.currentX, tempY-1):
+                tempY -= 1
+            else:
+                break
+
+        self.reachedBottom()
+    
+    def reachedBottom(self):
+        for i in range(4):
+            x = self.currentX + self.currentShape.getX()
+            y = self.currentY + self.currentShape.getY()
+            self.setShapeAt(x, y, self.currentShape)
+
+            self.clearLines()
+
+            self.spawnShape()
+    
+    def clearLines(self):
+        fullRows = []
+        linesCleared = 0
+
+        for i in range(20):
+            full = True
+            for j in range(10):
+                if self.getShapeAt(j, i) = blocks.empty:
+                    full = False
+
+            if full:
+                fullRows.append(i)
+                linesCleared += 1
+            
+        for i in fullRows:
+            for j in range(i, 20):
+                for k in range(10):
+                    self.setShapeAt(k, j, self.getShapeAt(k, j+1))
+
+        if linesCleared > 0:
+            self.totalLinesCleared += 1
+            self.update()
+           
+
+    def drawSquare(self, painter, x, y, shape):        
+        colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
+                      0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
+
+        color = QColor(colorTable[shape])
+        painter.fillRect(x + 1, y + 1, 50 - 2, 
+            50 - 2, color)
+
+        painter.setPen(color.lighter())
+        painter.drawLine(x, y + 50 - 1, x, y)
+        painter.drawLine(x, y, x + 50 - 1, y)
+
+        painter.setPen(color.darker())
+        painter.drawLine(x + 1, y + 50 - 1,
+            x + 50 - 1, y + 50 - 1)
+        painter.drawLine(x + 50 - 1, 
+            y + 50 - 1, x + 50 - 1, y + 1)
 
 
